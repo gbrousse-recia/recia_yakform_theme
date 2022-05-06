e93860dbeb1699a24d265b3267eb2cbd8d60d46c
gbrousse-recia <gregory.brousse@recia.fr>
2022-05-06 16:51:54 +0200

    fix

--- template.php
+++ template.php
@@ -1,25 +1,20 @@
 <?php
 function yaktheme_reciaform_preprocess_page(&$variables) {
-    echo $_SERVER['SERVER_NAME'].PHP_EOL;
     $domainCss = '';
     switch($_SERVER['SERVER_NAME']){
         //test cases
         case 'test-lycee.giprecia.net':
-            echo 'case test-lycee.giprecia.net'.PHP_EOL;
-            $domainCss = path_to_theme().'/sub/ent.css';
+            $domainCss = path_to_theme().'/css/sub/ent.css';
             break;
         case 'test-clg18.giprecia.net':
-            echo 'case test-clg18.giprecia.net'.PHP_EOL;
-            $domainCss = path_to_theme().'/sub/recia.css';
+            $domainCss = path_to_theme().'/css/sub/recia.css';
             break;
         //prod cases
         case 'ent.recia.fr':
-            echo 'case ent.recia.fr'.PHP_EOL;
-            $domainCss = path_to_theme().'/sub/recia.css';
+            $domainCss = path_to_theme().'/css/sub/recia.css';
             break;
     }
     if($domainCss!=''){
-        echo 'domain css defined : '.$domainCss.PHP_EOL;
         $options = array(
         'group' => CSS_THEME,
         'preprocess' => FALSE,
