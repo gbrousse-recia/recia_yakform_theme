098d7263fdd2542ff095cf57e65b037f959d72a9
gbrousse-recia <gregory.brousse@recia.fr>
2022-05-06 16:43:24 +0200

    test

--- template.php
+++ template.php
@@ -1,19 +1,26 @@
 <?php
 function yaktheme_reciaform_preprocess_page(&$variables) {
     echo $_SERVER['SERVER_NAME'];
+    $domainCss = '';
     switch($_SERVER['SERVER_NAME']){
         //test cases
         case 'test-lycee.giprecia.net':
-            drupal_add_css(drupal_get_path('theme', 'yaktheme_recia') .'css/sub/ent.css');
+            $domainCss = path_to-theme().'css/sub/ent.css';
             break;
         case 'test-clg18.giprecia.net':
-            drupal_add_css(drupal_get_path('theme', 'yaktheme_recia') .'css/sub/recia.css');
+            $domainCss = path_to-theme().'css/sub/recia.css';
             break;
         //prod cases
         case 'ent.recia.fr':
-            drupal_add_css(drupal_get_path('theme', 'yaktheme_recia') .'css/sub/recia.css');
+            $domainCss = path_to-theme().'css/sub/recia.css';
             break;
     }
-    $variables['styles'] = drupal_get_css();
-    print_r($variables['styles']);
+    if($domainCss!=''){
+        $options = array(
+        'group' => CSS_THEME,
+        'preprocess' => FALSE,
+        );
+        drupal_add_css($domainCss, $options);
+        $variables['styles'] = drupal_get_css();
+    }
 }
