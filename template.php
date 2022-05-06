b1d8b17faca168d62761e55b3d172163cdeff093
gbrousse-recia <gregory.brousse@recia.fr>
2022-05-06 16:18:26 +0200

    Fix : template.php

--- template.php
+++ template.php
@@ -1,17 +1,17 @@
 <?php
-function yakthemeReciaform_preprocess(&$variables) {
+function yaktheme_recia_preprocess_page(&$variables) {
     
     switch($_SERVER['SERVER_NAME']){
         //test cases
         case 'test-lycee.giprecia.net':
-            drupal_add_css('css/sub/ent.css');
+            drupal_add_css(drupal_get_path('theme', 'yaktheme_recia') .'css/sub/ent.css');
             break;
         case 'test-clg18.giprecia.net':
-            drupal_add_css('css/sub/recia.css');
+            drupal_add_css(drupal_get_path('theme', 'yaktheme_recia') .'css/sub/recia.css');
             break;
         //prod cases
         case 'ent.recia.fr':
-            drupal_add_css('css/sub/recia.css');
+            drupal_add_css(drupal_get_path('theme', 'yaktheme_recia') .'css/sub/recia.css');
             break;
 
     }
