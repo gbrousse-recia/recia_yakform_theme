6c88a800b26dd94dbf836eca3454de069130c11b
gbrousse-recia <gregory.brousse@recia.fr>
2022-05-23 16:29:09 +0200

    Fix

--- templates/footer-gip.tpl.php
+++ templates/footer-gip.tpl.php
@@ -2,7 +2,7 @@
     <div id="recia-footer-wrapper">
         <div class="recia-footer-coord">
             <?= $image ?>
-            <p class="footer-contact mt-3 hidden-pod"><span class="footer-contact-title"></span><br>
+            <p class="footer-contact mt-3 hidden-pod">
                 151 rue de la Juine<br>
                 45160 Olivet - France<br>
                 <a href="https://www.recia.fr" target="_blank">www.recia.fr</a><br>
